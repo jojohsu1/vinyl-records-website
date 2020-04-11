27718b07a7e390ae4f67a95ebe8bfe0b5fc2514a
jojo hsu <jojohsu1@gmail.com>
2020-04-11 21:32:28 +0800

    add onclick

--- js/data/new.js
+++ js/data/new.js
@@ -1,6 +1,7 @@
 export const newList = [{
         "id": "1",
         "url": "../../music/sibelius-violin_concerto-03.mp3",
+        "link": "../../albums/heifetz_sibelius.html",
         "img": "../../images/steve-johnson-ToF3lD9z2Vk-unsplash.jpg",
         "time": "11:51",
         "title": "Sibelius Violin Concerto in D Minor, Op. 47",
@@ -9,6 +10,7 @@ export const newList = [{
     {
         "id": "2",
         "url": "../../music/beethoven-piano_sonata_no.14_moonlight-sonata.mp3",
+        "link": "../../albums/barenboim_beethoven.html",
         "img": "../../images/jeremy-bishop-1braZySlEKA-unsplash.jpg",
         "time": "16:50",
         "title": "Beethoven Piano Sonata No.14 Moonlight Sonata",
@@ -17,6 +19,7 @@ export const newList = [{
     {
         "id": "3",
         "url": "../../music/dvorak-symphony_no. 9_from_the_new_world.mp3",
+        "link": "../../albums/abbado_berliner_dvorak.html",
         "img": "../../images/toa-heftiba-jwraMZtxBHs-unsplash.jpg",
         "time": "13:18",
         "title": "Dvorak Symphony No. 9 From The New World",
@@ -25,6 +28,7 @@ export const newList = [{
     {
         "id": "4",
         "url": "../../music/sarasate-carmen_fantasy_op. 25.mp3",
+        "link": "../../albums/perlman_sarasate.html",
         "img": "../../images/julio-rionaldo-xIoze9dH4WI-unsplash.jpg",
         "time": "13:42",
         "title": "Pablo De Sarasate - Carmen Fantasy Op. 25",

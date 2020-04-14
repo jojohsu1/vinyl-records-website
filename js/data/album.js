932e6cbd8549a0457c8e280d0e450f4dc625618b
jojo hsu <jojohsu1@gmail.com>
2020-04-14 17:59:26 +0800

    add album data

---
+++ js/data/album.js
@@ -0,0 +1,176 @@
+export const albumList = [{
+        "id": "1",
+        "status": "",
+        "genre": "piano",
+        "img": "../../images/jake-noren-xwEbGBA-hdk-unsplash.jpg",
+        "link": "../../albums/argerich_bach.html",
+        "artist": "Martha Argerich",
+        "title": "Bach : Toccata in C Minor, Partita No. 2, English Suite No. 2",
+        "composer": "Bach",
+        "price": "2600"
+    },
+    {
+        "id": "2",
+        "status": "",
+        "genre": "piano",
+        "img": "../../images/priscilla-du-preez-XZEYdvJznMQ-unsplash.jpg",
+        "link": "../../albums/lorem_ravel.html",
+        "artist": "Lorem ipsum dolor",
+        "title": "Ravel: Miroirs-Alborada del gracioso",
+        "composer": "Ravel",
+        "price": "980"
+    },
+    {
+        "id": "3",
+        "status": "",
+        "genre": "piano",
+        "img": "../../images/nathan-anderson-AU06xTIT8xw-unsplash.jpg",
+        "link": "../../albums/rubinstein_chopin.html",
+        "artist": "Arthur Rubinstein",
+        "title": "Chopin: Nocturnes",
+        "composer": "Chopin",
+        "price": "1800"
+    },
+    {
+        "id": "4",
+        "status": "New",
+        "genre": "piano",
+        "img": "../../images/jeremy-bishop-1braZySlEKA-unsplash.jpg",
+        "link": "../../albums/barenboim_beethoven.html",
+        "artist": "Daniel Barenboim",
+        "title": "Beethoven Piano Sonata No.14 Moonlight Sonata",
+        "composer": "Beethoven",
+        "price": "1800"
+    },
+    {
+        "id": "5",
+        "status": "",
+        "genre": "strings",
+        "img": "../../images/alireza-nazari-a8lSSWjK7PQ-unsplash.jpg",
+        "link": "../../albums/amet_sarasate.html",
+        "artist": "Amet consectetur",
+        "title": "Pablo De Sarasate: Zigeunerweisen",
+        "composer": "Sarasate",
+        "price": "1200"
+    },
+    {
+        "id": "6",
+        "status": "",
+        "genre": "strings",
+        "img": "../../images/kael-bloom-TYcgGyD5580-unsplash.jpg",
+        "link": "../../albums/yo-yo_ma_bumble-Bee.html",
+        "artist": "Yo-Yo Ma",
+        "title": "The Flight Of The Bumble-Bee",
+        "composer": "Nikolai Rimski-Korsakof",
+        "price": "2200"
+    },
+    {
+        "id": "7",
+        "status": "",
+        "genre": "strings",
+        "img": "../../images/viktor-jakovlev-mtNweauBsMQ-unsplash.jpg",
+        "link": "../../albums/fournier_bach.html",
+        "artist": "Pierre Fournier",
+        "title": "Bach: Unaccompanied Cello Suites",
+        "composer": "Bach",
+        "price": "1850"
+    },
+
+    {
+        "id": "8",
+        "status": "",
+        "genre": "strings",
+        "img": "../../images/johan-anblick-EWEI69jpVx8-unsplash.jpg",
+        "link": "../../albums/tatiyana_vivaldi.html",
+        "artist": "Tatiyana nulla rerum",
+        "title": "Vivaldi：The Four Seasons",
+        "composer": "Vivaldi",
+        "price": "720"
+    },
+    {
+        "id": "9",
+        "status": "",
+        "genre": "strings",
+        "img": "../../images/steve-johnson-0j9u-ItaGxM-unsplash.jpg",
+        "link": "../../albums/benny_tseng_tchaikovsky.html",
+        "artist": "Yu-Chien Tseng",
+        "title": "Tchaikovsky Violin Concerto in D major, Op. 35",
+        "composer": "Tchaikovsky",
+        "price": "1980"
+    },
+    {
+        "id": "10",
+        "status: "
+        ",
+        "genre": "strings",
+        "img": "../../images/joel-wyncott-WYiIkQFclA4-unsplash.jpg",
+        "link": "../../albums/benny_tseng_sibelius.html",
+        "artist": "Yu-Chien Tseng",
+        "title": "Sibelius Violin Concerto in D Minor, Op. 47",
+        "composer": "Sibelius",
+        "price": "1500"
+    },
+    {
+        "id": "11",
+        "status": "New",
+        "genre": "strings",
+        "img": "../../images/steve-johnson-ToF3lD9z2Vk-unsplash.jpg",
+        "link": "../../albums/heifetz_sibelius.html",
+        "artist": "Heifetz",
+        "title": "Sibelius Violin Concerto in D Minor, Op. 47",
+        "composer": "Sibelius",
+        "price": "1500"
+    },
+    {
+        "id": "12",
+        "status": "New",
+        "genre": "strings",
+        "img": "../../images/julio-rionaldo-xIoze9dH4WI-unsplash.jpg",
+        "link": "../../albums/perlman_sarasate.html",
+        "artist": "Itzhak Perlman",
+        "title": "Pablo De Sarasate - Carmen Fantasy Op. 25",
+        "composer": "Sarasate",
+        "price": "1500"
+    },
+    {
+        "id": "13",
+        "status": "New",
+        "genre": "symphony",
+        "img": "../../images/toa-heftiba-jwraMZtxBHs-unsplash.jpg",
+        "link": "../../albums/abbado_berliner_dvorak.html",
+        "artist": "Abbado · Berliner Philharmoniker",
+        "title": "Dvorak Symphony No. 9 From The New World",
+        "composer": "Dvorak",
+        "price": "500"
+    },
+    {
+        "id": "14",
+        "status": "",
+        "genre": "pop",
+        "img": "../../images/nick-karvounis-tp_aLk0ngME-unsplash.jpg",
+        "link": "../../albums/lopsum_close-to-You.html",
+        "artist": "Lopsum dolor",
+        "title": "Close to You",
+        "price": "1350"
+    },
+    {
+        "id": "15",
+        "status": "",
+        "genre": "pop",
+        "img": "../../images/blake-guidry-9wWMLO0JqHQ-unsplash.jpg",
+        "link": "../../albums/lopsum_close-to-You.html",
+        "artist": "Adipisicing aliquid",
+        "title": "PERSONA",
+        "price": "788"
+    },
+    {
+        "id": "16",
+        "status": "",
+        "genre": "Jazz",
+        "img": "../../images/jorge-zapata-ajLlK5qL6Ic-unsplash.jpg",
+        "link": "../../albums/ullam_et-cetera.html",
+        "artist": "Ullam libero",
+        "title": "Et cetera",
+        "price": "1080"
+    }
+];
